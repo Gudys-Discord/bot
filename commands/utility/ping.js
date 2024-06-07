887782f8f076b47ecb67e478df3b0d67a7610625
Thiago <thiagozevedo3@gmail.com>
2024-06-06 21:20:48 -0300

    refactor main file

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with the latency!')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const timeTaken = Date.now() - interaction.createdTimestamp + interaction.client.ws.ping;
+		await interaction.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
 	},
 };
--- main.js
+++ main.js
@@ -1,46 +1,64 @@
 require('dotenv').config();
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
-const path = require('node:path')
+const { promises: fs } = require('fs');
+const path = require('path');
+const { Client: DiscordClient, Collection, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions
-    ]
-});
+/**
+ * @param {import('discord.js').Client} client - The Discord client.
+ */
 
-client.commands = new Collection();
+class Client extends DiscordClient {
+    constructor(options) {
+        super(options);
+        this.commands = new Collection();
+    }
+}
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+async function main() {
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildMessageReactions
+        ]
+    });
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    client.once('ready', async () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+
+        const foldersPath = path.join(__dirname, 'commands');
+        const commandFolders = await fs.readdir(foldersPath);
+
+        for (const folder of commandFolders) {
+            const commandsPath = path.join(foldersPath, folder);
+            const commandFiles = (await fs.readdir(commandsPath)).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const filePath = path.join(commandsPath, file);
+                const command = require(filePath);
+                if ('data' in command && 'execute' in command) {
+                    command.execute = command.execute.bind(client);
+                    client.commands.set(command.data.name, command);
+                } else {
+                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+                }
+            }
         }
-    }
+
+        const eventsPath = path.join(__dirname, 'events');
+        const eventFiles = (await fs.readdir(eventsPath)).filter(file => file.endsWith('.js'));
+
+        for (const file of eventFiles) {
+            const filePath = path.join(eventsPath, file);
+            const event = require(filePath);
+            if (event.once) {
+                client.once(event.name, (...args) => event.execute.apply(event, args));
+            } else {
+                client.on(event.name, (...args) => event.execute.apply(event, args));
+            }
+        }
+    });
+
+    client.login(process.env.TOKEN).catch(console.error);
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
-}
-
-client.login(process.env.TOKEN);
+main().catch(console.error);
