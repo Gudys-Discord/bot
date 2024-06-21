9241e8276e8f0d4a6aeb864e560752da4c1845b8
Thiago <thiagozevedo3@gmail.com>
2024-06-20 22:25:10 -0300

    debug

--- commands/vip/vip.js
+++ commands/vip/vip.js
@@ -176,7 +176,7 @@ module.exports = {
                     const filter = m => m.author.id === interaction.user.id;
                     const messageCollector = interaction.channel.createMessageCollector({ filter, time: 15000 });
                     messageCollector.on('collect', async m => {
-                        const days = parseInt(m.content);
+                        const days = console.log(parseInt(m.content));
                         if (isNaN(days)) {
                             await m.reply({ content: 'Por favor, digite um número válido.', ephemeral: true });
                         } else {
