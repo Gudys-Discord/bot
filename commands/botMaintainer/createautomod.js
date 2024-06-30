2df7f9587a86f66753cd3d1bf9fa485903dec55d
Thiago <thiagozevedo3@gmail.com>
2024-06-30 11:22:27 -0300

    Update createautomod.js

--- commands/botMaintainer/createautomod.js
+++ commands/botMaintainer/createautomod.js
@@ -11,10 +11,19 @@ module.exports = {
 
     async execute(interaction) {
         // This command creates an automod rule in all 9 servers that are in the devServer variable
-        const devServers = ['1256971261310013471', '1256971183514062899', '1256971018267005010',
+        const devServers = [
+            '1256971261310013471', '1256971183514062899',
+            '1256971018267005010', '737173603107340310',
             '1240514457298276402', '1240720918314221679',
             '1240720983908683828', '1240720956461158533',
             '1240721047347789884', '1240721072890970275'
+        ];
+
+        const automodRules = [
+            { type: 'flagged-words', count: 2 },
+            { type: 'spam-messages', count: 2 },
+            { type: 'spam-mentions', count: 2 },
+            { type: 'keywords', count: 3 }
         ];
 
         for (const guildId of devServers) {
@@ -22,7 +31,11 @@ module.exports = {
             const channel = guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'));
 
             if (channel) {
-                await channel.send({ content: 'Regras de automoderação criadas com sucesso!' });
+                for (const rule of automodRules) {
+                    for (let i = 0; i < rule.count; i++) {
+                        await channel.send({ content: `Automod rule created: ${rule.type}` });
+                    }
+                }
             }
         }
     }
