e31390ecbb213d5c81f5f84c7ff00550cbe16865
Thiago <thiagozevedo3@gmail.com>
2024-07-01 12:43:31 -0300

    Update fibonacci.js

--- commands/botMaintainer/fibonacci.js
+++ commands/botMaintainer/fibonacci.js
@@ -11,12 +11,16 @@ module.exports = {
 
     async execute(interaction) {
         const index = interaction.options.getInteger('index');
-        const message = await interaction.channel.send({ content: 'Calculating...' });
+        const message = await interaction.reply({ content: 'Calculating...', ephemeral: false });
 
-        function fibonacci(n, memo = {}) {
-            if (memo[n]) return memo[n];
-            if (n <= 1) return BigInt(n);
-            return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+        function fibonacci(n) {
+            let a = BigInt(0), b = BigInt(1), temp;
+            for (let i = 0; i < n; i++) {
+                temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
         }
 
         const result = fibonacci(index).toString();
@@ -28,13 +32,11 @@ module.exports = {
 
         if (result.length <= 2000) {
             embed.addFields({ name: 'Result', value: `\`\`\`${result}\`\`\`` });
-            await message.delete();
-            await interaction.reply({ embeds: [embed] });
+            await interaction.update({ embeds: [embed] });
         } else {
             const fileContent = result;
             const file = new AttachmentBuilder(Buffer.from(fileContent, 'utf-8'), { name: `fibonacci_${index}.txt` });
-            await message.delete();
-            await interaction.reply({ content: 'Result too long, sending as file...', files: [file] });
+            await interaction.update({ content: 'Result too long, sending as file...', files: [file] });
         }
     },
 };
